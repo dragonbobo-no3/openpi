1d7dcc2ee3e9b4654b70daac7a3f449223e877d3
jemotor <690979417@qq.com>
2025-09-11 13:46:55 +0800

    fix action_in_proj shaoe invalid

--- src/openpi/training/weight_loaders.py
+++ src/openpi/training/weight_loaders.py
@@ -91,7 +91,10 @@ def _merge_params(loaded_params: at.Params, params: at.Params, *, missing_regex:
     result = {}
     for k, v in flat_loaded.items():
         if k in flat_ref:
-            result[k] = v.astype(flat_ref[k].dtype) if v.dtype != flat_ref[k].dtype else v
+            if v.shape == flat_ref[k].shape:
+                result[k] = v.astype(flat_ref[k].dtype)
+            else:
+                continue
 
     flat_loaded.clear()
 
