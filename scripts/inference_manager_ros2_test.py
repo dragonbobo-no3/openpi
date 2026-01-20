8375f1a4c8ff433e94c3d73a7ae2bb8da5aeb575
KleistLiu <gmvonkleist@gmail.com>
2026-01-20 17:22:13 +0800

    modify executor thread

--- scripts/inference_manager_ros2_test.py
+++ scripts/inference_manager_ros2_test.py
@@ -814,9 +814,10 @@ def main(args=None):
     rclpy.init(args=args)
     node = InferenceManager()
     try:
-        executor = MultiThreadedExecutor(num_threads=os.cpu_count() or 4)
-        executor.add_node(node)
-        executor.spin()
+        # executor = MultiThreadedExecutor(num_threads=os.cpu_count() or 4)
+        # executor.add_node(node)
+        # executor.spin()
+        rclpy.spin(node)
     except KeyboardInterrupt:
         pass
     finally:
